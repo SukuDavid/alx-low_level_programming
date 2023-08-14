6411a0491ebd6632caa047414f42ac2b2f7dda36
sukudavid <sk.david802@gmail.com>
2023-08-14 13:11:26 -0700

    all

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+putchar("");
+putchar("\n");
+return 0;
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void print_to_98(int n)
+{
+while(n<98)
+{
+printf("%d, \n", n);
+}
+}
+
+int main ()
+{
+print_to_98(0);
+}
