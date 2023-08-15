0f40a7a21e806d884f6f46734ab31242a4e30ab7
sukudavid <sk.david802@gmail.com>
2023-08-15 07:34:20 -0700

    all

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -17,8 +17,11 @@ for (i = 2; i < 50; i++)
 {
 fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 if (i == 49)
+{
 printf("%ld\n", fibonacci[i]);
+}
 else
+{
 printf("%ld, ", fibonacci[i]);
 }
 return (0);
