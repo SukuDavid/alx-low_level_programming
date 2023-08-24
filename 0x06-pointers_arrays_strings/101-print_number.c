f2ea6811edf6d8841ca2857c84da211b28ad5fc0
sukudavid <sk.david802@gmail.com>
2023-08-24 12:54:40 -0700

    all

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,5 @@
  */
 void print_number(int n)
 {
-unsigned int i;
-
-if (n < 0)
-{
-_putchar('-');
-i = -n;
-}
-else
-i = n;
-if (i / 10)
-print_number(i / 10);
-_putchar(i % 10 + '0');
+_putchar(n);
 }
